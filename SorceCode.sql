2825913541acff50c73365c4c18cc5423880abda
Deanna Baccus <97615077+baccus02@users.noreply.github.com>
2023-12-10 23:34:08 -0500

    Add files via upload

---
+++ SorceCode.sql
@@ -0,0 +1,141 @@
+USE HealthcareData;
+INSERT INTO HealthcareRecords (FirstName, LastName, Gender, Age, Weight, Height, HealthHistory)
+VALUES ('Jane', 'Doe', 1, 28, 65.0, 165.0, 'No known allergies'),
+ ('John', 'Smith', 0, 35, 78.5, 182.2, 'Diabetes'),
+ ('Emily', 'Jones', 1, 26, 62.0, 168.3, 'No known allergies'),
+ ('Michael', 'Brown', 0, 42, 88.3, 175.7, 'Hypertension'),
+ ('Jessica', 'Taylor', 1, 31, 54.1, 160.5, 'Asthma'),
+ ('William', 'Davis', 0, 22, 74.2, 170.0, 'None'),
+ ('Sarah', 'Wilson', 1, 29, 68.4, 158.8, 'Migraines'),
+ ('David', 'Moore', 0, 47, 92.9, 180.4, 'High cholesterol'),
+ ('Emma', 'Taylor', 1, 36, 70.0, 165.0, 'Anemia'),
+ ('Daniel', 'Anderson', 0, 19, 85.0, 190.5, 'None'),
+ ('Olivia', 'Thomas', 1, 40, 61.2, 162.1, 'No known allergies'),
+ ('Matthew', 'Jackson', 0, 58, 77.3, 172.0, 'Heart Disease'),
+ ('Sophia', 'White', 1, 33, 63.7, 169.5, 'Epilepsy'),
+ ('Max', 'Alexander', 1, 47, 82.9, 182.3, 'High cholesterol'),
+('Nora', 'Bailey', 0, 21, 54.8, 159.6, 'No known allergies'),
+('Owen', 'Gray', 1, 38, 77.6, 175.0, 'Asthma'),
+('Paula', 'West', 0, 50, 61.7, 160.2, 'Diabetes'),
+('Quinn', 'Ford', 1, 29, 69.9, 171.5, 'Hypertension'),
+('Rita', 'Harper', 0, 43, 65.3, 167.1, 'Migraines'),
+('Seth', 'Holmes', 1, 56, 90.4, 176.5, 'No known allergies'),
+('Tara', 'Boyd', 0, 34, 62.9, 163.8, 'Anemia'),
+('Uriel', 'Mason', 1, 27, 74.1, 169.4, 'Heart Disease'),
+('Vera', 'Robertson', 0, 46, 68.6, 158.7, 'None'),
+('Wyatt', 'Brooks', 1, 19, 85.2, 182.9, 'No known allergies'),
+('Xena', 'Cox', 0, 39, 56.1, 154.3, 'High cholesterol'),
+('Yusuf', 'Diaz', 1, 52, 83.8, 178.2, 'Diabetes'),
+('Zoey', 'Fleming', 0, 24, 60.7, 166.5, 'Hypertension'),
+('Aaron', 'Snyder', 1, 44, 79.0, 172.3, 'Asthma'),
+('Beth', 'Morris', 0, 31, 67.4, 161.0, 'No known allergies'),
+('Caleb', 'Pearson', 1, 48, 91.5, 184.6, 'Migraines'),
+('Diana', 'Reid', 0, 26, 57.2, 157.5, 'Heart Disease'),
+('Ethan', 'Perry', 1, 37, 88.6, 179.3, 'None'),
+('Fiona', 'Stone', 0, 53, 63.0, 164.7, 'Diabetes'),
+('George', 'Nichols', 1, 29, 76.3, 173.2, 'Hypertension'),
+('Hannah', 'Atkinson', 0, 41, 62.4, 168.0, 'Asthma'),
+('Ian', 'Collier', 1, 33, 87.1, 180.8, 'No known allergies'),
+ ('James', 'Harris', 0, 27, 80.8, 177.8, 'Diabetes'),
+ ('Isabella', 'Martin', 1, 21, 56.9, 159.4, 'No known allergies'),
+ ('Christopher', 'Garcia', 0, 53, 89.6, 174.3, 'Hypertension'),
+ ('Ava', 'Clark', 1, 45, 64.3, 167.6, 'None'),
+ ('Joshua', 'Rodriguez', 0, 38, 83.5, 185.2, 'High cholesterol'),
+ ('Mia', 'Lewis', 1, 30, 58.0, 163.2, 'Asthma'),
+ ('Andrew', 'Lee', 0, 24, 76.1, 175.9, 'None'),
+ ('Laura', 'Green', 0, 38, 72.5, 167.6, 'No known allergies'),
+('Nathan', 'Edwards', 1, 20, 68.4, 175.2, 'Migraines'),
+('Olivia', 'Murphy', 0, 55, 76.3, 165.4, 'Hypertension'),
+('Peter', 'Harris', 1, 45, 85.7, 178.4, 'Diabetes'),
+('Rachel', 'Wood', 0, 33, 64.1, 162.9, 'Asthma'),
+('Samuel', 'Thompson', 1, 29, 90.3, 182.7, 'None'),
+('Uma', 'Phillips', 0, 47, 59.8, 160.0, 'High cholesterol'),
+('Victor', 'Campbell', 1, 22, 83.5, 170.1, 'Anemia'),
+('Wendy', 'Mitchell', 0, 31, 70.4, 168.3, 'Heart Disease'),
+('Xavier', 'Roberts', 1, 58, 88.1, 183.5, 'None'),
+('Yolanda', 'Carter', 0, 26, 61.9, 159.5, 'Epilepsy'),
+('Zachary', 'Wright', 1, 49, 77.3, 176.8, 'Diabetes'),
+('Amanda', 'Lopez', 0, 37, 65.4, 164.2, 'No known allergies'),
+('Brian', 'Hill', 1, 19, 69.5, 171.3, 'Asthma'),
+('Cynthia', 'Torres', 0, 42, 73.6, 169.9, 'None'),
+('Derek', 'Sanders', 1, 28, 82.7, 177.5, 'High cholesterol'),
+('Elena', 'Ramirez', 0, 34, 58.8, 158.1, 'Migraines'),
+('Felix', 'Barnes', 1, 48, 86.2, 180.0, 'Hypertension'),
+('Gina', 'Rivera', 0, 23, 61.1, 155.2, 'None'),
+('Hugo', 'Cook', 1, 52, 79.3, 172.4, 'Heart Disease'),
+('Irene', 'Morgan', 0, 39, 67.7, 167.8, 'Diabetes'),
+('Justin', 'Bell', 1, 25, 88.9, 175.6, 'Epilepsy'),
+('Kathy', 'Murphy', 0, 46, 62.8, 160.3, 'No known allergies'),
+('Leonard', 'Bailey', 1, 30, 81.3, 182.2, 'Asthma'),
+('Monica', 'Rivera', 0, 53, 74.5, 168.0, 'None'),
+('Oscar', 'Coleman', 1, 21, 85.0, 176.7, 'High cholesterol'),
+('Pamela', 'Jenkins', 0, 41, 59.0, 157.9, 'Hypertension'),
+('Quincy', 'Perry', 1, 36, 82.1, 179.1, 'Heart Disease'),
+('Rebecca', 'Brooks', 0, 27, 66.5, 163.3, 'Diabetes'),
+('Steven', 'Reed', 1, 54, 77.8, 170.5, 'No known allergies'),
+('Tina', 'Powell', 0, 32, 63.2, 160.8, 'Migraines'),
+('Vince', 'Long', 1, 44, 89.4, 181.6, 'Asthma'),
+('Wanda', 'Patterson', 0, 29, 57.6, 158.5, 'None'),
+('Xander', 'Hughes', 1, 48, 93.5, 183.2, 'High cholesterol'),
+ ('Madison', 'Walker', 1, 50, 67.8, 170.3, 'Migraines'),
+ ('Ethan', 'Hall', 0, 60, 90.2, 169.0, 'Heart Disease'),
+ ('Abigail', 'Allen', 1, 34, 55.5, 160.7, 'Diabetes'),
+ ('Alexander', 'Young', 0, 48, 86.4, 182.9, 'Hypertension'),
+ ('Elizabeth', 'Hernandez', 1, 37, 62.5, 164.1, 'No known allergies'),
+ ('Noah', 'King', 0, 29, 75.0, 178.6, 'None'),
+ ('Ella', 'Wright', 1, 41, 59.3, 161.3, 'High cholesterol'),
+ ('Ryan', 'Lopez', 0, 25, 81.6, 176.5, 'Asthma'),
+ ('Avery', 'Hill', 1, 32, 66.0, 155.0, 'No known allergies'),
+ ('Benjamin', 'Scott', 0, 46, 82.7, 173.4, 'Epilepsy'),
+ ('Alice', 'Smith', 0, 24, 55.2, 162.5, 'Diabetes'),
+ ('Bob', 'Johnson', 1, 31, 89.3, 175.3, 'No known allergies'),
+ ('Carol', 'Williams', 0, 45, 63.7, 158.2, 'Hypertension'),
+ ('Lara', 'Gibson', 0, 32, 58.3, 162.4, 'None'),
+ ('Julia', 'Armstrong', 0, 49, 70.3, 165.9, 'High cholesterol'),
+ ('David', 'Brown', 1, 36, 77.1, 180.4, 'Asthma'),
+ ('Eva', 'Jones', 0, 22, 60.5, 165.0, 'Migraines'),
+ ('Frank', 'Miller', 1, 50, 90.2, 172.7, 'High cholesterol'),
+ ('Grace', 'Davis', 0, 29, 64.3, 160.0, 'None'),
+ ('Henry', 'Garcia', 1, 41, 85.6, 168.9, 'Epilepsy'),
+ ('Isabel', 'Martinez', 0, 35, 70.2, 170.2, 'Anemia'),
+ ('Jack', 'Hernandez', 1, 28, 88.5, 175.9, 'No known allergies');
+ALTER TABLE UserAccounts MODIFY HashedPassword VARCHAR(64) NOT NULL;
+
+INSERT INTO UserAccounts (Username, HashedPassword, Role)
+VALUES 
+('user1', SHA2('user1Password', 256), 'H'),
+('user2', SHA2('user2Password', 256), 'R');
+
+ CREATE USER 'groupH_user'@'wasp' IDENTIFIED BY '&Tr1ple$ecureP@ssw0rd!987';
+CREATE USER 'groupR_user'@'wasp' IDENTIFIED BY '&Tr1ple$ecureP@ssw0rd!987';
+
+GRANT ALL PRIVILEGES ON HealthcareData.HealthcareRecords TO 'groupH_user'@'localhost';
+GRANT SELECT (id, Gender, Age, Weight, Height, HealthHistory) ON HealthcareData.HealthcareRecords TO 'groupR_user'@'localhost';
+FLUSH PRIVILEGES;
+
+ALTER TABLE HealthcareData.HealthcareRecords ADD COLUMN RecordHash VARCHAR(64);
+UPDATE HealthcareData.HealthcareRecords
+SET RecordHash = SHA2(CONCAT(IFNULL(FirstName,''), IFNULL(LastName,''), IFNULL(Gender,''), IFNULL(Age,''), IFNULL(Weight,''), IFNULL(Height,''), IFNULL(HealthHistory,'')), 256);
+SELECT *, SHA2(CONCAT(IFNULL(FirstName,''), IFNULL(LastName,''), IFNULL(Gender,''), IFNULL(Age,''), IFNULL(Weight,''), IFNULL(Height,''), IFNULL(HealthHistory,'')), 256) AS NewHash
+FROM HealthcareData.HealthcareRecords
+HAVING NewHash = RecordHash;
+
+DESCRIBE HealthcareData.HealthcareRecords;
+
+ALTER TABLE HealthcareData.HealthcareRecords 
+ADD COLUMN EncryptedGender VARBINARY(128), 
+ADD COLUMN EncryptedAge VARBINARY(128); 
+
+UPDATE HealthcareData.HealthcareRecords
+SET EncryptedGender = AES_ENCRYPT(Gender, 'encryptionwasp'),
+    EncryptedAge = AES_ENCRYPT(Age, 'encryptionwasp');
+
+ALTER TABLE HealthcareData.HealthcareRecords 
+DROP COLUMN Gender, 
+DROP COLUMN Age;
+
+SELECT AES_DECRYPT(EncryptedGender, 'encryption_key') AS Gender,
+       AES_DECRYPT(EncryptedAge, 'encryption_key') AS Age
+FROM HealthcareData.HealthcareRecords;
+
+ 
